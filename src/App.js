37d73d3f0903cd69bf6359f4a95e696a7e6a9820
bofmar <bofmar@outlook.com>
2022-11-28 14:47:35 +0200

    Add suffle function

--- src/App.js
+++ src/App.js
@@ -3,6 +3,16 @@ import Card from './components/Card';
 import './App.css';
 
 function App() {
+
+  function shuffle(arr) {
+    for (let i = arr.length - 1; i > 0; i--) {
+      const j = Math.floor(Math.random() * (i + 1));
+      [arr[i], arr[j]] = [arr[j], arr[i]];
+    }
+    return arr;
+
+  }
+
   return (
     <div className="App">
       <div className='cards'>
